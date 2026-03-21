81a639606266792ad9dea51580108659863103b5
Jonathan A. McCormick, Jr. <67705789+JonathanMcCormickJr@users.noreply.github.com>
2026-03-21 14:51:17 -0500

    progress

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,7 +1,27 @@
-use axum::response::IntoResponse;
+use crate::domain::{ data_stores::UserStore, email::Email, password::Password};
+use axum::{Json, response::IntoResponse};
 use reqwest::StatusCode;
 
-// For now we will simply return a 200 (OK) status code.
-pub async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+pub async fn login(user_store: impl UserStore, Json(body): Json<serde_json::Value>) -> impl IntoResponse {
+    // JSON object must have username and password fields, otherwise return 422 Unprocessable Entity. Use the `parse` functions in email.rs and password.rs to validate the fields.
+    let email = body.get("email").and_then(|v| v.as_str());
+            let password = body.get("password").and_then(|v| v.as_str());
+    if email.is_none() || password.is_none() {
+        return (StatusCode::UNPROCESSABLE_ENTITY, "Missing email or password").into_response();
+    }
+    let email = email.unwrap();
+    let password = password.unwrap();
+    if Email::parse(email).is_err() {
+        return (StatusCode::UNPROCESSABLE_ENTITY, "Invalid email").into_response();
+    }
+    if Password::parse(password).is_err() {
+        return (StatusCode::UNPROCESSABLE_ENTITY, "Invalid password").into_response();
+    }
+
+    // Check if the email and password match a user in the database. If not, return 400 Bad Request with an appropriate error message.
+    if user_store.get_user(&Email::parse(email).unwrap()).await.is_err() {
+        return (StatusCode::BAD_REQUEST, "Invalid email or password").into_response();
+    }
+
+    (StatusCode::OK, "Login successful").into_response()
 }
--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,10 +1,29 @@
-use crate::helpers::TestApp;
+use crate::helpers::{get_random_email, TestApp};
+use serde_json::json;
 
 #[tokio::test]
-async fn login_returns_ok() {
+async fn should_return_422_if_malformed_credentials() {
     let app = TestApp::new().await;
 
-    let response = app.post_login().await;
+    let body = json!({
+        "email": "not-an-email",
+        "password": "short",
+    });
 
-    assert_eq!(response.status().as_u16(), 200);
+    let response = app.post_login(&body).await;
+
+    assert_eq!(response.status().as_u16(), 422);
+}
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    // Call the log-in route with invalid credentials and assert that a
+    // 400 HTTP status code is returned along with the appropriate error message. 
+    let app = TestApp::new().await;
+    let body = json!({
+        "email": get_random_email(),
+        "password": "short",
+    });
+    let response = app.post_login(&body).await;
+    assert_eq!(response.status().as_u16(), 400);
 }
